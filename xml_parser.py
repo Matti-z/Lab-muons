64d4fe277896930606d31d3c97d8e8c9705a2ce2
ABoldetti <a.boldetti@campus.unimib.it>
2025-11-27 08:19:03 +0100

    fix parser

--- xml_parser.py
+++ xml_parser.py
@@ -13,13 +13,16 @@ def xml_digitizer_parser(path:str = "")->list:
     tree = parse("prova_xml.xml")
     root = tree.getroot()
 
+    events = []
+
     for event in root.findall("event"):
         trace = event.find("trace").text
         id = event.get("id")
         time_stamp = event.get("timestamp")
         clock_time = event.get("clocktime")
+        events.append([int(x) for x in trace.split()])
         
-        return [int(x) for x in trace.split()]
+    return events
     
 
 if __name__ == "__main__":
