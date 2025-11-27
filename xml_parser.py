ed41c107f05b277999b8edeea83566684051ceef
ABoldetti <a.boldetti@campus.unimib.it>
2025-11-27 08:17:40 +0100

    xml plot

--- xml_parser.py
+++ xml_parser.py
@@ -1,6 +1,10 @@
 from xml.etree.ElementTree import parse
 from os.path import isfile
+import matplotlib.pyplot as plt
 
+
+#TODO: Conversion sampling units -> time
+#TODO: Convertion digital value -> volts
 
 def xml_digitizer_parser(path:str = "")->list:
 
@@ -19,4 +23,6 @@ def xml_digitizer_parser(path:str = "")->list:
     
 
 if __name__ == "__main__":
-    print(xml_digitizer_parser("prova_xml.xml"))
+    a =xml_digitizer_parser("prova_xml.xml")
+    plt.plot(a)
+    plt.show()
