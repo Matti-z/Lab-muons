a415fc00377de4d23e7e9585064ac9afc961fc29
ABoldetti <a.boldetti@campus.unimib.it>
2025-11-28 11:40:02 +0100

    xml parser time conversion

--- xml_parser.py
+++ xml_parser.py
@@ -1,10 +1,13 @@
 from xml.etree.ElementTree import parse
 from os.path import isfile
 import matplotlib.pyplot as plt
+from numpy import arange
 
 
 #TODO: Conversion sampling units -> time
 #TODO: Convertion digital value -> volts
+#! studiare CAEN wave pulse per vedere se da terminale funziona
+
 
 def xml_digitizer_parser(path:str = "")->list:
 
@@ -14,18 +17,29 @@ def xml_digitizer_parser(path:str = "")->list:
     root = tree.getroot()
 
     events = []
+    digitizer = root.find("digitizer")
+    frequency = float(digitizer.find("frequency").get("hz"))
+
+    settings = root.find("settings")
+    size = float(settings.find("window").get("size"))
+
+    time = arange( size)/frequency
+
 
     for event in root.findall("event"):
         trace = event.find("trace").text
         id = event.get("id")
         time_stamp = event.get("timestamp")
         clock_time = event.get("clocktime")
-        events.append([int(x) for x in trace.split()])
         
-    return events
+        events.append([int(x) for x in trace.split()])
+    
+    return events , time , frequency
     
 
 if __name__ == "__main__":
-    a =xml_digitizer_parser("prova_xml.xml")
-    plt.plot(a)
-    plt.show()
+    a , b , c=xml_digitizer_parser("prova_xml.xml")
+
+    for i in a:
+        plt.plot(b , i)
+        plt.show()
