f54772bf82511947225babc00a7706b640ac0714
ABoldetti <a.boldetti@campus.unimib.it>
2026-01-08 22:10:23 +0100

    devo ottimizzarlo :(

--- digitizer/root_parser.py
+++ digitizer/root_parser.py
@@ -5,6 +5,7 @@ import uproot
 import numpy as np
 from pathlib import Path
 from array import array
+from gc import collect
 
 
 default_dir = str(Path(".").absolute()).split("string_A")[0]+"Lab-muons/"
@@ -25,7 +26,7 @@ def csv_to_root(csv_folder, output_root):
         return
     
     root_output_file =  universal_dir(output_root)
-
+    
 
     output_root_file = ROOT.TFile(root_output_file, "UPDATE")
     tree = ROOT.TTree("timestamp", "recollection of timestamps")
@@ -38,24 +39,35 @@ def csv_to_root(csv_folder, output_root):
                 break
     
 
-    data = np.empty(1024 , dtype=np.float32)
     file_list = os.listdir(csv_folder)
     
     for i in range(len(file_list)):
-        event = [array("f" , [0]) for _ in range(1024)]
+
+        perc: int = int(round(i /  len(file_list) * 30))
+        string: str = (
+            "[" + "#" * perc + "-" * (30 - perc) + "]\t" + "\t" + str(i) + "\t" + str(len(file_list)))
+        print("\r" + string, end="", flush=True)
+        data = np.genfromtxt(csv_folder +"/"+  file_list[i], delimiter=',' , dtype=np.uint16)
+        event = array("f" , [0])
         data_branch_name = f"event_{str(file_list[i]).removesuffix(".csv")}"
-        data_branch_title = f"event_{str(file_list[i]).removesuffix(".csv")} %\F"
+        data_branch_title = f"event_{str(file_list[i]).removesuffix(".csv")} /F"
         tree.Branch(data_branch_name, event, data_branch_title)
 
-        for j in range(1024):
-            event[j][0] = data[j]
-        tree.Fill()
+        for j in range(len(data)):
+            event[0] = data[j]
+            tree.Fill()
         
+        # tree.Write("", ROOT.TObject.kOverwrite)
+        del data
+        del event
+        del data_branch_name
+        del data_branch_title
+        collect()
 
     output_root_file.Write()
     output_root_file.Close()
 
 if __name__ == "__main__":
-    csv_folder = "./your_csv_folder"  # Change this path
-    output_root = "./combined_data.root"
+    csv_folder = "csv/natale"  # Change this path
+    output_root = "Data/root_folder/natale.root"
     csv_to_root(csv_folder, output_root)
