49a0e7ba38ac3f7d9413202d4c9ba6c7ef789f9e
ABoldetti <a.boldetti@campus.unimib.it>
2026-02-27 22:17:23 +0100

    modified full_chain

--- project/full_chain.py
+++ project/full_chain.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 from pathlib import Path
 from root_to_timestamp import process_root_files, root_settings_to_csv
-# from drive_sync import sync_local_folder_to_drive
+
 
 
 
@@ -10,6 +10,12 @@ from root_to_timestamp import process_root_files, root_settings_to_csv
 default_dir = str(Path(".").absolute()).split("/Lab-muons")[0]+"/Lab-muons/"
 universal_dir = lambda path: default_dir + path
 
+
+
+
+
+invertito = True
+drive = False
 xml_path = universal_dir("big_data/error_prop_100ns.xml")
 xml_filename = xml_path.split('/')[-1].removesuffix(".xml")
 csv_path = universal_dir("Data/timestamp/"+xml_filename+".csv")
@@ -25,20 +31,22 @@ os.makedirs(os.path.dirname(xml_path), exist_ok=True)
 os.makedirs(os.path.dirname(root_path), exist_ok=True)
 os.makedirs(os.path.dirname(csv_settings_path), exist_ok=True)
 
-
-result = subprocess.run([universal_dir("project/parser"), xml_path, root_path], capture_output=False)
-if result.returncode != 0:
-    print(result.stderr)
-process_root_files(root_path , csv_path)
-root_settings_to_csv( root_path , csv_settings_path)
+if invertito:
+    result = subprocess.run([universal_dir("project/root_parser"), xml_path, root_path], capture_output=False)
+    if result.returncode != 0:
+        print(result.stderr)
+    process_root_files(root_path , csv_path)
+    root_settings_to_csv( root_path , csv_settings_path)
+else:
+    result = subprocess.run([universal_dir("project/csv_parser"), xml_path, csv_path , csv_settings_path], capture_output=False)
 
 subprocess.run(['git', 'add', universal_dir("Data/timestamp/*") , universal_dir("Data/settings/*")], capture_output=False)
 subprocess.run(['git', 'commit', '-m', 'Update timestamp data ' + xml_filename], capture_output=False)
 subprocess.run(['git', 'push'], capture_output=False)
 
 
-
-# copy files to drive, if not configure, comment it out
-# sync_local_folder_to_drive()
+if drive:
+    from drive_sync import sync_local_folder_to_drive
+    sync_local_folder_to_drive()
 
 
