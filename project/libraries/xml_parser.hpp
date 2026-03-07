21f890bb25c081977d7fa5be0557066841a403e3
ABoldetti <a.boldetti@campus.unimib.it>
2026-03-07 16:47:45 +0100

    bug fixing

--- project/libraries/xml_parser.hpp
+++ project/libraries/xml_parser.hpp
@@ -4,6 +4,6 @@
 #include <pugixml.hpp>
 
 void setting_parser( std::string starter, std::ifstream& in , pugi::xml_document &digitizer);
-void event_parser( std::ifstream& in , pugi::xml_node &events);
+void event_parser( std::ifstream& in , pugi::xml_node &events , pugi::xml_document &history);
 void settings_to_csv(pugi::xml_document &digitizer, pugi::xml_document &settings , std::ofstream& cfg);
 void xml_doc(std::ifstream &in, pugi::xml_document &digitizer , std::string xmlClass);
