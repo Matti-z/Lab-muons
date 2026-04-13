19e361c84b1b58aadd0b82a2d102962d039d1562
ABoldetti <a.boldetti@campus.unimib.it>
2026-04-13 18:28:27 +0200

    whatevs

--- digitizer/full_chain.py
+++ digitizer/full_chain.py
@@ -21,7 +21,7 @@ universal_dir = lambda path: default_dir + path
 
 file = "multichannel_parser"
 drive = True
-xml_path = universal_dir("big_data/salt_27_03_2026_16_36.xml")
+xml_path = universal_dir("big_data/salt_10_04_2026_17_02.xml")
 xml_filename = xml_path.split('/')[-1].removesuffix(".xml")
 csv_folder = universal_dir("Data/timestamp/")
 csv_settings_folder = universal_dir("Data/settings/")
