8cea1b369d70a5da9706a3e2e074d05e0c9195ce
Matti-z <Matti-z+github@users.noreply.github.com>
2026-09-04 11:57:17 +0200

    deadzones with 75 cm and inutile 73cm

--- simulazioni/tia/g4/convert_csv_to_h5.py
+++ simulazioni/tia/g4/convert_csv_to_h5.py
@@ -14,7 +14,7 @@ import numpy as np
 
 DEFAULT_ROOTS = (
     # Path(__file__).parent / "results" / "gmp",
-    Path(__file__).parent / "results" / "mgp" / "75cmm",
+    Path(__file__).parent / "results" / "mgp" / "73cm",
 )
 CHUNK_SIZE = 10_000
 
